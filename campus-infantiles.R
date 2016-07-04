aab7e2a75ff79584db4078fe5c224dcedd973e05
JJ Merelo <jjmerelo@gmail.com>
2016-07-04 20:01:02 +0200

    Añade R
    También gráfico para usarlo en la presentación

---
+++ campus-infantiles.R
@@ -0,0 +1,16 @@
+## ----setup, include=FALSE, cache=FALSE-----------------------------------
+
+require(ggplot2) 
+require(ggthemes)
+
+data <- read.csv("grupos-de-edad.csv")
+
+## ----evolucion, cache=FALSE,echo=FALSE-----------------------------------
+ggplot()+geom_line(data=data,aes(x=AÑO,y=as.numeric(X7.8),color="7-8"))+geom_line(data=data,aes(x=AÑO,y=as.numeric(X9.10),color="9-10"))+geom_line(data=data,aes(x=AÑO,y=as.numeric(X11.),color="11+"))+geom_point(data=data,aes(x=AÑO,y=as.numeric(X7.8),color="7-8"))+geom_point(data=data,aes(x=AÑO,y=as.numeric(X9.10),color="9-10"))+geom_point(data=data,aes(x=AÑO,y=as.numeric(X11.),color="11+"))+ylab("Porcentaje de chicas")+theme_tufte()
+ggsave('por-edad.png')
+
+## ----resumen, cache=FALSE,echo=FALSE-------------------------------------
+library(knitr)
+data.guay <- data.frame(edad.hasta.8 = data$X7.8, de.9.a.10 = data$X9.10, de.11.en.adelante = data$X11.)
+kable(summary(data.guay))
+
