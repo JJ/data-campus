568e5e7f6fd7edb51ffda94d810c12cf4007ae41
JJ Merelo <jjmerelo@gmail.com>
2017-09-13 18:26:56 +0200

    Ahora en barras

--- matrículas-etsiit-género.R
+++ matrículas-etsiit-género.R
@@ -1,5 +1,6 @@
 require(ggplot2) 
 require(ggthemes)
+library(tidyr)
 
 data <- read.csv("grados-género-2012-2017.csv")
 data$total <- data$Mujeres+data$Hombres
@@ -7,8 +8,11 @@ data$porcentaje <- data$Mujeres/data$total
 
 
 ggplot()+geom_line(data=data,aes(x=Año,y=porcentaje,color=Carrera))+ylab("Porcentaje de mujeres")+ guides(color=guide_legend(title="Carreras"))+theme_tufte()
+
 ggplot()+geom_line(data=data,aes(x=Año,y=Mujeres,color=Carrera))+ylab("Número de mujeres")+ guides(color=guide_legend(title="Carreras"))+theme_tufte()
 
-
-
+data.v2 <-  gather(data,"Género","Valor",3:4)
+data.gii <- data.v2[data.v2$Carrera=='Informática',]
+ggplot(data.gii,aes(x=Año,y=Valor,fill=factor(Género)))+geom_bar(stat = "identity")
+ggplot(data.gii,aes(x=Año,y=Valor/total,fill=factor(Género)),position="fill")+geom_bar(stat = "identity")+geom_text(aes(label=Género),position=position_stack(vjust=0.5),size=10)+theme_tufte()
 
