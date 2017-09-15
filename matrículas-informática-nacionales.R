87151619bbd855becc78a37d6d6165b7d4543dc9
JJ Merelo <jjmerelo@gmail.com>
2017-09-15 11:54:20 +0200

    Comparación con Málaga

--- matrículas-informática-nacionales.R
+++ matrículas-informática-nacionales.R
@@ -13,6 +13,13 @@ ggplot(data.percentage,aes(x=Curso,y=porcentaje,fill=porcentaje))+geom_bar(stat=
 data.Granada <- data[data$Universidad=='Granada',]
 ggplot(data.Granada,aes(x=Curso,y=Valor,group=Genero,color=Genero))+geom_line() +theme_tufte() + theme(axis.text.x = element_text(angle = 90, hjust = 1))+ggtitle('Universidad de Granada')
 
-data.percentage <- spread(data.Granada,Genero,Valor)
-data.percentage$porcentaje <- data.percentage$" Mujeres"/data.percentage$" Total"
-ggplot(data.percentage,aes(x=Curso,y=porcentaje,fill=porcentaje))+geom_bar(stat="Identity") +theme_tufte() + theme(axis.text.x = element_text(angle = 90, hjust = 1))
+data.percentage.UGR <- spread(data.Granada,Genero,Valor)
+data.percentage.UGR$porcentaje <- data.percentage.UGR$" Mujeres"/data.percentage.UGR$" Total"
+ggplot(data.percentage.UGR,aes(x=Curso,y=porcentaje,fill=porcentaje))+geom_bar(stat="Identity") +theme_tufte() + theme(axis.text.x = element_text(angle = 90, hjust = 1))
+
+data.Malaga <- data[data$Universidad=='Málaga',]
+ggplot(data.Malaga,aes(x=Curso,y=Valor,group=Genero,color=Genero))+geom_line() +theme_tufte() + theme(axis.text.x = element_text(angle = 90, hjust = 1))+ggtitle('Universidad de Málaga')
+
+data.percentage.UMA <- spread(data.Malaga,Genero,Valor)
+data.percentage.UMA$porcentaje <- data.percentage.UMA$" Mujeres"/data.percentage.UMA$" Total"
+ggplot(data.percentage.UMA,aes(x=Curso,y=porcentaje,fill=porcentaje))+geom_bar(stat="Identity") +theme_tufte() + theme(axis.text.x = element_text(angle = 90, hjust = 1))
