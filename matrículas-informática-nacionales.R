7b3bc807628005f1eaf04df0bc42eefe89ab7d8e
JJ Merelo <jjmerelo@gmail.com>
2017-09-27 07:37:46 +0200

    Porcentajes de Andalucía Oriental

--- matrículas-informática-nacionales.R
+++ matrículas-informática-nacionales.R
@@ -30,4 +30,18 @@ data.percentage.UMA <- spread(data.Malaga,Genero,Valor)
 data.percentage.UMA$porcentaje <- data.percentage.UMA$" Mujeres"/data.percentage.UMA$" Total"
 ggplot(data.percentage.UMA,aes(x=Curso,y=porcentaje,fill=porcentaje))+geom_bar(stat="Identity") +theme_tufte() + theme(axis.text.x = element_text(angle = 90, hjust = 1))
 
-ggplot()+geom_line(data=data.percentage.UMA,aes(x=Curso,y=porcentaje,color='blue',group=1))+geom_line(data=data.percentage.UGR,aes(x=Curso,y=porcentaje,color='red',group=1)) +theme_tufte() + theme(axis.text.x = element_text(angle = 90, hjust = 1))
+data.Jaen <- data[data$Universidad=='Jaén',]
+ggplot(data.Jaen,aes(x=Curso,y=Valor,group=Genero,color=Genero))+geom_line() +theme_tufte() + theme(axis.text.x = element_text(angle = 90, hjust = 1))+ggtitle('Universidad de Jaén')
+
+data.percentage.Jaen <- spread(data.Jaen,Genero,Valor)
+data.percentage.Jaen$porcentaje <- data.percentage.Jaen$" Mujeres"/data.percentage.Jaen$" Total"
+ggplot(data.percentage.Jaen,aes(x=Curso,y=porcentaje,fill=porcentaje))+geom_bar(stat="Identity") +theme_tufte() + theme(axis.text.x = element_text(angle = 90, hjust = 1))
+
+data.UAL <- data[data$Universidad=='Almería',]
+ggplot(data.UAL,aes(x=Curso,y=Valor,group=Genero,color=Genero))+geom_line() +theme_tufte() + theme(axis.text.x = element_text(angle = 90, hjust = 1))+ggtitle('Universidad de Almería')
+
+data.percentage.UAL <- spread(data.UAL,Genero,Valor)
+data.percentage.UAL$porcentaje <- data.percentage.UAL$" Mujeres"/data.percentage.UAL$" Total"
+ggplot(data.percentage.UAL,aes(x=Curso,y=porcentaje,fill=porcentaje))+geom_bar(stat="Identity") +theme_tufte() + theme(axis.text.x = element_text(angle = 90, hjust = 1))
+
+ggplot()+geom_line(data=data.percentage.UMA,aes(x=Curso,y=porcentaje,color='UMA',group=1))+geom_line(data=data.percentage.UGR,aes(x=Curso,y=porcentaje,color='UGR',group=1))+geom_line(data=data.percentage.Jaen,aes(x=Curso,y=porcentaje,color='UJA',group=1))+geom_line(data=data.percentage.UAL,aes(x=Curso,y=porcentaje,color='UAL',group=1)) +theme_tufte() + theme(axis.text.x = element_text(angle = 90, hjust = 1))
