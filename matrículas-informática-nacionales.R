311f1c75af45d451e29ff72c46dabfcc79ed8722
JJ Merelo <jjmerelo@gmail.com>
2017-10-05 08:03:07 +0200

    Cálculo porcentajes último año

--- matrículas-informática-nacionales.R
+++ matrículas-informática-nacionales.R
@@ -4,7 +4,7 @@ library(tidyr)
 library(dplyr)
 
 data <- read.csv("mujeres-en-informática-desde-1986-sin-UNED.csv", fileEncoding="latin1")
-data.por.uni <- data[data$Genero==' Total',]
+data.por.uni <- data[data$Genero=='Total',]
 data.por.uni <- data.por.uni[data.por.uni$Universidad!='Univ. Públicas Presenciales',]
 data.por.uni <- data.por.uni[data.por.uni$Universidad!='Univ. Públicas No Presenciales',]
 data.por.uni <- data.por.uni[data.por.uni$Universidad!='Unis sin UNED',]
@@ -13,8 +13,19 @@ data.total <- data[data$Universidad=='Unis sin UNED',]
 ggplot(data.total,aes(x=Curso,y=Valor,group=Genero,color=Genero))+geom_line() +theme_tufte() + theme(axis.text.x = element_text(angle = 90, hjust = 1))
 
 data.percentage <- spread(data.total,Genero,Valor)
-data.percentage$porcentaje <- data.percentage$" Mujeres"/data.percentage$" Total"
+data.percentage$porcentaje <- data.percentage$Mujeres/data.percentage$Total
 ggplot(data.percentage,aes(x=Curso,y=porcentaje,fill=porcentaje))+geom_bar(stat="Identity") +theme_tufte() + theme(axis.text.x = element_text(angle = 90, hjust = 1))
+
+data <-  spread(data,Genero,Valor)
+data$porcentaje <- data$Mujeres/data$Total
+mutate.deltap <- data %>% group_by(Universidad) %>% mutate(delta.p=c(NA,diff(porcentaje)))
+mutate.delta <- data %>% group_by(Universidad) %>% mutate(delta=c(NA,diff(Mujeres)))
+data$delta.p <- mutate.deltap$delta.p
+data$delta <- mutate.delta$delta
+data.last <- data[data$Curso=="2016-2017",]
+ggplot(data.last,aes(x=reorder(Universidad,porcentaje),y=porcentaje,fill=porcentaje))+geom_bar(stat="Identity") +theme_tufte() + theme(axis.text.x = element_text(angle = 90, hjust = 1))
+ggplot(data.last,aes(x=reorder(Universidad,delta.p),y=delta.p,fill=delta))+geom_bar(stat="Identity") +theme_tufte() + theme(axis.text.x = element_text(angle = 90, hjust = 1))
+
 
 data.Granada <- data[data$Universidad=='Granada',]
 ggplot(data.Granada,aes(x=Curso,y=Valor,group=Genero,color=Genero))+geom_line() +theme_tufte() + theme(axis.text.x = element_text(angle = 90, hjust = 1))+ggtitle('Universidad de Granada')
