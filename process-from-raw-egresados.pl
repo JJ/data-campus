850daddee89c50a3f4287d028720d48c606dd62a
JJ Merelo <jjmerelo@gmail.com>
2017-10-13 07:39:21 +0200

    Graduados en informática añadidos

---
+++ process-from-raw-egresados.pl
@@ -0,0 +1,36 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+
+use v5.14;
+
+use File::Slurper qw(read_lines);
+
+my $file_name = shift || "egresados-en-informática-desde-2009.csv"; # Archivo en formato raw sin líneas iniciales
+
+my @file = read_lines($file_name, "latin1");
+
+die "No se lee el fichero $file_name" if !@file;
+
+@file = @file[5..$#file-8];
+
+my @years = grep(/\d/, split(/;/, shift @file));
+
+shift @file; # Cabeceras
+say "Universidad,Curso,Género,Valor";
+while ( @file ) {
+  my $uni_line = shift @file;
+  my ($nombre_universidad, @foo) = split(";",$uni_line);
+  $nombre_universidad =~ s/"//g;
+  my $data_line = shift @file;
+  $data_line =~ s/"//g;
+  my @data = split(";",$data_line);
+  for (my $d=1; $d <= $#data; $d++) {
+    my ($data_number) = ($data[$d] =~ /(\d+)/);
+    next if !$data_number;
+    say "$nombre_universidad, $years[($d-1)/2], ", ($d % 2)?"Total":"Mujeres", ", $data_number";
+  }
+}
+
+
